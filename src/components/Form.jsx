be6373d0d0deabe58f3a36238fa140613b42f678
rocky123454321 <rockyjayducusin3@gmail.com>
2025-12-18 18:45:05 +0800

    Update site

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ export default function Form({ formData, handleChange }) {
   return (
     <div className="flex">
       <div className="w-1/3 p-4 space-y-2 border-r overflow-y-auto h-screen">
-        <h2 className="font-bold text-lg mb-4">Harvard Cv generator</h2>
+        <h2 className="font-bold text-lg mb-4">Harvard CV Generator</h2>
        
       </div>
     </div>
