721b257fe49fbbbd96474542dc95c026eca389d0
rocky123454321 <rockyjayducusin3@gmail.com>
2025-12-18 22:03:47 +0800

    Update site

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Cv_maker_live/', // <-- MUST match repo name
+  base: '/Cv_maker_live/', // Must match your repo name EXACTLY (case-sensitive)
   plugins: [react()],
 })
